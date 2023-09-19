dd2bb9da4dd80591e84b657307000eb9ae49c79c
ornob90 <oornob49@gmail.com>
2023-09-19 14:50:09 +0600

    setting up categories

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ mealData, setCurCategory }) => {
+  return (
+    <div className="w-[80%] mx-auto flex justify-center gap-5 text-white mt-10 items-center flex-wrap">
+      {mealData?.map(({ idCategory, strCategory }) => (
+        <div
+          key={idCategory}
+          onClick={() => setCurCategory(strCategory)}
+          className="bg-rose-600 px-4 py-1 rounded-lg"
+        >
+          <Link to={`/meals/${strCategory}`}>{strCategory}</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav className="w-[90%] mx-auto flex gap-10 text-white text-xl pt-4 justify-center">
+      <Link to="/meals/Beef">Meal</Link>
+    </nav>
+  );
 };
 
 export default Header;
